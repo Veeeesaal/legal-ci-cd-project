74e43b2eac9e3c2090f2f2b0509365e0bce959c6
Vishal <vr5485281@gmail.com>
2026-02-17 16:38:40 +0530

    Clean commit without venv

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes import health
+
+app = FastAPI(title="Legal CI/CD App")
+
+app.include_router(health.router)
+
+@app.get("/")
+def root():
+    return {"message": "Legal App Running Successfully"}
---
+++ tests/test_health.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+# Project root ko Python path me add karo
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "healthy"}
+
